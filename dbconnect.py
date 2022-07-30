566f4684f16c24d4f678aa7c9772aa7fd934c8fd
pmconwa2 <37543728+pmconwa2@users.noreply.github.com>
2022-07-29 23:13:12 -0400

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -2,10 +2,10 @@ import psycopg2
 
 
 def connection():
-    conn = psycopg2.connect(host="localhost",
-                           database="nfl",
-                           user="postgres",
-                           password="oceanic46&!",
+    conn = psycopg2.connect(host="ec2-44-208-88-195.compute-1.amazonaws.com",
+                           database="d20atckuunjd6a",
+                           user="btifnirlvheggd",
+                           password="456dae0c256c66d5f8b454351412c5dedd3cfa6f9e696a8c229954549fe2c166",
                             )
     c = conn.cursor()
     return c, conn
